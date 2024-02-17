7f9ecb2bde9e0cf729084fc18049cd4b166f6602
Alvaro Bernal <alvaro-122010@hotmail.com>
2024-02-17 09:11:47 -0500

    Update Dockerfile and configuration files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hmr: {
-      clientPort: 8080
+      clientPort: 8080,
     },
-    port: 8080,
+    port: 5173,
     watch: {
-      usePolling: true
+      usePolling: true,
     },
   },
   plugins: [react()],
-})
+});
